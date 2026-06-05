b60ce5c775f53cad89817939f05611cac943700d
Michał Perczak <perczak.michal.pl@gmail.com>
2026-06-05 18:13:16 +0200

    base

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/az-region")
+async def get_region_data():
+    try:
+        az = requests.get(
+            "http://169.254.169.254/latest/meta-data/placement/availability-zone",
+            timeout=2,
+        ).text
+        region = az[:-1]
+        return {"region": region, "availability_zone": az}
+
+    except requests.exceptions.RequestException:
+        # Fallback for when you test this locally on your own computer
+        return {
+            "error": "Could not connect to AWS Metadata API.",
+            "message": "API accessible only from within an EC2 instance.",
+        }
